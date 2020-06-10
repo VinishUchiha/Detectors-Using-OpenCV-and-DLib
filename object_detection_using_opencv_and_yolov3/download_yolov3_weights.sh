9c36231359498a5d3b70671e78381102a7b44491
Vinish <vinishvimal7@gmail.com>
2020-06-10 22:01:52 +0530

    Add files via upload

---
+++ object_detection_using_opencv_and_yolov3/download_yolov3_weights.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download the YOLOv3 weights and cfg file
+
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
